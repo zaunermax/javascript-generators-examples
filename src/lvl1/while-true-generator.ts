a1b87de04381b438c6037e0cd2cc75fa5d17de49
Maximilian Zauner <zaunermax@gmail.com>
2024-04-21 21:01:46 +0200

    feat(comments): added code comments

--- src/lvl1/while-true-generator.ts
+++ src/lvl1/while-true-generator.ts
@@ -1,11 +1,13 @@
 function* generateIds() {
 	let value = 0;
 
+	// ⚡️⚡️ looks scary - fine with generators though ⚡️⚡️
 	while (true) {
 		yield (value += 1);
 	}
 }
 
+// manually "pulling" values
 const iterator = generateIds();
 
 console.log(iterator.next().value);
